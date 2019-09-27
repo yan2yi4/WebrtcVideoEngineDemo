4f792d76d3d378bcfe97d40f50d5714b1ef937d7
Yi Yan <yiyiyiyan@outlook.com>
2019-09-27 12:27:10 +0800

    added sender

--- webrtc_videoengine_demo/main.cc
+++ webrtc_videoengine_demo/main.cc
@@ -780,3 +780,390 @@ int main(int argc, char* argvc[])
 	
 	return 0;
 }
+/*
+int VideoEngineSample(void* window1, void* window2)
+{
+
+	int error = 0;
+
+	//
+	// Create a VideoEngine instance
+	//
+	webrtc::VideoEngine* ptrViE = NULL;
+	ptrViE = webrtc::VideoEngine::Create();
+	if (ptrViE == NULL)
+	{
+		printf("ERROR in VideoEngine::Create\n");
+		return -1;
+	}
+
+	//
+	// Init VideoEngine and create a channel
+	//
+	webrtc::ViEBase* ptrViEBase = webrtc::ViEBase::GetInterface(ptrViE);
+	if (ptrViEBase == NULL)
+	{
+		printf("ERROR in ViEBase::GetInterface\n");
+		return -1;
+	}
+
+	error = ptrViEBase->Init();
+	if (error == -1)
+	{
+		printf("ERROR in ViEBase::Init\n");
+		return -1;
+	}
+
+	webrtc::ViERTP_RTCP* ptrViERtpRtcp =
+		webrtc::ViERTP_RTCP::GetInterface(ptrViE);
+	if (ptrViERtpRtcp == NULL)
+	{
+		printf("ERROR in ViERTP_RTCP::GetInterface\n");
+		return -1;
+	}
+
+	int videoChannel = -1;
+	error = ptrViEBase->CreateChannel(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViEBase::CreateChannel\n");
+		return -1;
+	}
+
+	//
+	// List available capture devices, allocate and connect.
+	//
+	webrtc::ViECapture* ptrViECapture =
+		webrtc::ViECapture::GetInterface(ptrViE);
+	if (ptrViEBase == NULL)
+	{
+		printf("ERROR in ViECapture::GetInterface\n");
+		return -1;
+	}
+
+	const unsigned int KMaxDeviceNameLength = 128;
+	const unsigned int KMaxUniqueIdLength = 256;
+	char deviceName[KMaxDeviceNameLength];
+	memset(deviceName, 0, KMaxDeviceNameLength);
+	char uniqueId[KMaxUniqueIdLength];
+	memset(uniqueId, 0, KMaxUniqueIdLength);
+
+	printf("Available capture devices:\n");
+	int captureIdx = 0;
+	for (captureIdx = 0;
+		captureIdx < ptrViECapture->NumberOfCaptureDevices();
+		captureIdx++)
+	{
+		memset(deviceName, 0, KMaxDeviceNameLength);
+		memset(uniqueId, 0, KMaxUniqueIdLength);
+
+		error = ptrViECapture->GetCaptureDevice(captureIdx, deviceName,
+			KMaxDeviceNameLength, uniqueId,
+			KMaxUniqueIdLength);
+		if (error == -1)
+		{
+			printf("ERROR in ViECapture::GetCaptureDevice\n");
+			return -1;
+		}
+		printf("\t %d. %s\n", captureIdx + 1, deviceName);
+	}
+	printf("\nChoose capture device: ");
+
+	if (scanf("%d", &captureIdx) != 1)
+	{
+		printf("Error in scanf()\n");
+		return -1;
+	}
+	getchar();
+	captureIdx = captureIdx - 1; // Compensate for idx start at 1.
+
+	error = ptrViECapture->GetCaptureDevice(captureIdx, deviceName,
+		KMaxDeviceNameLength, uniqueId,
+		KMaxUniqueIdLength);
+	if (error == -1)
+	{
+		printf("ERROR in ViECapture::GetCaptureDevice\n");
+		return -1;
+	}
+
+	int captureId = 0;
+	error = ptrViECapture->AllocateCaptureDevice(uniqueId, KMaxUniqueIdLength,
+		captureId);
+	if (error == -1)
+	{
+		printf("ERROR in ViECapture::AllocateCaptureDevice\n");
+		return -1;
+	}
+
+	error = ptrViECapture->ConnectCaptureDevice(captureId, videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViECapture::ConnectCaptureDevice\n");
+		return -1;
+	}
+
+	error = ptrViECapture->StartCapture(captureId);
+	if (error == -1)
+	{
+		printf("ERROR in ViECapture::StartCapture\n");
+		return -1;
+	}
+
+	//
+	// RTP/RTCP settings
+	//
+
+	error = ptrViERtpRtcp->SetRTCPStatus(videoChannel,
+		webrtc::kRtcpCompound_RFC4585);
+	if (error == -1)
+	{
+		printf("ERROR in ViERTP_RTCP::SetRTCPStatus\n");
+		return -1;
+	}
+
+	error = ptrViERtpRtcp->SetKeyFrameRequestMethod(
+		videoChannel, webrtc::kViEKeyFrameRequestPliRtcp);
+	if (error == -1)
+	{
+		printf("ERROR in ViERTP_RTCP::SetKeyFrameRequestMethod\n");
+		return -1;
+	}
+
+	error = ptrViERtpRtcp->SetRembStatus(videoChannel, true, true);
+	if (error == -1)
+	{
+		printf("ERROR in ViERTP_RTCP::SetTMMBRStatus\n");
+		return -1;
+	}
+
+
+	//
+	// Set up rendering
+	//
+	webrtc::ViERender* ptrViERender = webrtc::ViERender::GetInterface(ptrViE);
+	if (ptrViERender == NULL) {
+		printf("ERROR in ViERender::GetInterface\n");
+		return -1;
+	}
+
+
+	error
+		= ptrViERender->AddRenderer(captureId, window1, 0, 0.0, 0.0, 1.0, 1.0);
+	if (error == -1)
+	{
+		printf("ERROR in ViERender::AddRenderer\n");
+		return -1;
+	}
+
+	error = ptrViERender->StartRender(captureId);
+	if (error == -1)
+	{
+		printf("ERROR in ViERender::StartRender\n");
+		return -1;
+	}
+
+	error = ptrViERender->AddRenderer(videoChannel, window2, 1, 0.0, 0.0, 1.0,1.0);
+	if (error == -1){printf("ERROR in ViERender::AddRenderer\n");return -1;}
+
+	error = ptrViERender->StartRender(videoChannel);
+	if (error == -1){printf("ERROR in ViERender::StartRender\n");return -1;
+	}
+
+	//
+	// Setup codecs
+	//
+	webrtc::ViECodec* ptrViECodec = webrtc::ViECodec::GetInterface(ptrViE);
+	if (ptrViECodec == NULL)
+	{
+		printf("ERROR in ViECodec::GetInterface\n");
+		return -1;
+	}
+
+	VideoCodec videoCodec;
+
+	int numOfVeCodecs = ptrViECodec->NumberOfCodecs();
+	for (int i = 0; i<numOfVeCodecs; ++i)
+	{
+		if (ptrViECodec->GetCodec(i, videoCodec) != -1)
+		{
+			if (videoCodec.codecType == kVideoCodecVP8)
+				break;
+		}
+	}
+
+	videoCodec.targetBitrate = 64;
+	//videoCodec.minBitrate = 200;
+	videoCodec.minBitrate = 32;
+	//videoCodec.maxBitrate = 300;
+	videoCodec.maxBitrate = 128;
+	//videoCodec.maxFramerate = 25;
+	videoCodec.maxFramerate = 60;
+
+	error = ptrViECodec->SetSendCodec(videoChannel, videoCodec);
+	assert(error != -1);
+
+	error = ptrViECodec->SetReceiveCodec(videoChannel, videoCodec);
+	assert(error != -1);
+	//
+	// Address settings
+	//
+	webrtc::ViENetwork* ptrViENetwork =
+		webrtc::ViENetwork::GetInterface(ptrViE);
+	if (ptrViENetwork == NULL)
+	{
+		printf("ERROR in ViENetwork::GetInterface\n");
+		return -1;
+	}
+
+	VideoChannelTransport* video_channel_transport = NULL;
+	video_channel_transport = new VideoChannelTransport(
+		ptrViENetwork, videoChannel);
+
+
+	const char* ipAddress = "127.0.0.1";
+	uint16_t rtpPort = 6000;
+	std::cout << std::endl;
+	std::cout << "IP: " << ipAddress << std::endl;
+	std::cout << "Using rtp port: " << rtpPort << std::endl;
+	std::cout << std::endl;
+
+
+	error = video_channel_transport->SetLocalReceiver(rtpPort);
+	if (error == -1)
+	{
+		printf("ERROR in SetLocalReceiver\n");
+		return -1;
+	}
+
+	error = video_channel_transport->SetSendDestination(ipAddress, rtpPort);
+	if (error == -1)
+	{
+		printf("ERROR in SetSendDestination\n");
+		return -1;
+	}
+
+	error = ptrViEBase->StartReceive(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViENetwork::StartReceive\n");
+		return -1;
+	}
+
+	error = ptrViEBase->StartSend(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViENetwork::StartSend\n");
+		return -1;
+	}
+
+	printf("\n call started\n\n");
+	unsigned int total_bitrate_sent = 0;
+	unsigned int video_bitrate_sent = 0;
+	unsigned int fec_bitrate_sent = 0;
+	unsigned int nackBitrateSent = 0;
+
+	while (true){
+		int bandWidth = ptrViERtpRtcp->GetBandwidthUsage(videoChannel,
+			total_bitrate_sent, video_bitrate_sent, fec_bitrate_sent, nackBitrateSent);
+
+		printf("total_bitrate_sent: %4d kbits/s, video_bitrate_sent: %4d kbits/s\r",
+			total_bitrate_sent / 1024, video_bitrate_sent / 1024);
+
+	}
+
+	error = ptrViEBase->StopReceive(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViEBase::StopReceive\n");
+		return -1;
+	}
+
+	error = ptrViEBase->StopSend(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViEBase::StopSend\n");
+		return -1;
+	}
+
+	error = ptrViERender->StopRender(captureId);
+	if (error == -1)
+	{
+		printf("ERROR in ViERender::StopRender\n");
+		return -1;
+	}
+
+	error = ptrViERender->RemoveRenderer(captureId);
+	if (error == -1)
+	{
+		printf("ERROR in ViERender::RemoveRenderer\n");
+		return -1;
+	}
+
+	error = ptrViERender->StopRender(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViERender::StopRender\n");
+		return -1;
+	}
+
+	error = ptrViERender->RemoveRenderer(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViERender::RemoveRenderer\n");
+		return -1;
+	}
+
+	error = ptrViECapture->StopCapture(captureId);
+	if (error == -1)
+	{
+		printf("ERROR in ViECapture::StopCapture\n");
+		return -1;
+	}
+
+	error = ptrViECapture->DisconnectCaptureDevice(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViECapture::DisconnectCaptureDevice\n");
+		return -1;
+	}
+
+	error = ptrViECapture->ReleaseCaptureDevice(captureId);
+	if (error == -1)
+	{
+		printf("ERROR in ViECapture::ReleaseCaptureDevice\n");
+		return -1;
+	}
+
+	error = ptrViEBase->DeleteChannel(videoChannel);
+	if (error == -1)
+	{
+		printf("ERROR in ViEBase::DeleteChannel\n");
+		return -1;
+	}
+
+	delete video_channel_transport;
+	int remainingInterfaces = 0;
+	remainingInterfaces = ptrViECodec->Release();
+	remainingInterfaces += ptrViECapture->Release();
+	remainingInterfaces += ptrViERtpRtcp->Release();
+	remainingInterfaces += ptrViERender->Release();
+	remainingInterfaces += ptrViENetwork->Release();
+	remainingInterfaces += ptrViEBase->Release();
+
+	if (remainingInterfaces > 0)
+	{
+		printf("ERROR: Could not release all interfaces\n");
+		return -1;
+	}
+
+	bool deleted = webrtc::VideoEngine::Delete(ptrViE);
+	if (deleted == false)
+	{
+		printf("ERROR in VideoEngine::Delete\n");
+		return -1;
+	}
+
+	return 0;
+
+	}
+	*/
